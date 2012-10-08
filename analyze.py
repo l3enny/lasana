Added constants module, added debug option to most routines, fixed error in lineshape calculation, changed around how the settings are passed, started to implement a transitions database (not used yet).

analyze.py
@@ -1,75 +1,99 @@
 ﻿"""
-The mathematical component. Generates synthetic spectra. Runs comparison
+The mematical component. Generates synthetic spectra. Runs comparison
 algorithm. Tells you what you want to know. At present only considers
 Doppler broadening and pressure broadening. User must provide pressure
 broadening FWHM in angular frequency.
 """
 
 # Standard
-import math
+import math as m
 import matplotlib.pyplot as plt
+import matplotlib.text as mtext
 
 # Third Party
-import numpy as np
+import numpy as N
 from scipy import interpolate
 
 # Included
+from constants import *
 import lineshapes
+import misc
     
-def sigma(w0, w):
-    pass
+def sigma(initial, final, frequencies, Tg, debug=False, **settings):
+    A = 1.0216e7
+
+    f_0 = (final['E'] - initial['E'])/h
+    l = misc.vac2air(c/f_0)
+    fwhm_g = N.sqrt((8*m.log(2))*kB*Tg/(M_He*c**2)) * f_0
+    fwhm_a = settings['pressure'] * torr2hz
     
-def peak_find():
+    # Change this line to determine the lineshape that you desire
+    function = lineshapes.voigt(fwhm_g, fwhm_a)
+    profile = function(frequencies + settings['offset'])
+    return profile
+    # return 2 * N.pi * m.log(2) * final['g'] / initial['g'] * l**2 * A / 
+    
+def peak_find(debug=False):
     pass
 
-def get_temp(signal, wavelengths):
+def get_temp(signal, frequencies, debug=False):
     pass
     
-def quickndirty(signal, wavelengths):
-    torr = 7.0
-    torr2hz = 25.6e6
-    M = 4.002602 * 1.660538921e-27
-    c = 299792458
-    f_0 = c/1082.9091140e-9
-    kB = 1.3806503e-23
-    temperatures = np.linspace(200, 1400, 1200)
-    adjust = 0.05e9
-    wavelengths = wavelengths*1e9 + adjust
-    fwhm_p = torr*torr2hz
-    fwhm_d = np.sqrt((8*math.log(2))*kB*temperatures/(M*c**2)) * f_0
-    synthetic_profiles = np.zeros((len(temperatures), len(wavelengths)))
+def quickndirty(signal, initial, final, debug=False, **settings):
+
+    if debug:
+        check = [250, 450, 700, 1000, 1500, 1999]
+
+    Ti = 200
+    Tf = 1500
+    temperatures = N.linspace(Ti, Tf, Tf - Ti + 1)
+
+    freq_shift = (settings['mod_initial']*ma2hz, settings['mod_final']*ma2hz)
+    frequencies = N.linspace(freq_shift[0], freq_shift[1], settings['samples'])
+    
+    absorption_synthetic = N.zeros((len(temperatures), settings['samples']))
+    
     for i in range(len(temperatures)):
-        function = lineshapes.voigt(fwhm_d[i], fwhm_p)
-        # function = lineshapes.gaussian(fwhm_d[i])
-        synthetic_profiles[i, :] = function(wavelengths)
-    norms = np.array(np.max(synthetic_profiles, axis=1))
-    synthetic_profiles = synthetic_profiles/norms[:, np.newaxis]
+        absorption_synthetic[i, :] = sigma(initial, final, frequencies,
+                                           temperatures[i], **settings)
     
-    transmission = 1 - signal
-    # norms = np.max(transmission, axis=0)
-    # transmission = transmission/norms
-    # plt.contourf(transmission, levels=np.linspace(np.min(transmission), np.max(transmission), 100))
-    # plt.show()
+    # TODO: Eliminate normalization by using proper calculation of pathlength
+    # absorption
+    norms = N.array(N.max(absorption_synthetic, axis=1))
+    absorption_synthetic = absorption_synthetic/norms[:, N.newaxis]
+    
+    absorption = 1 - signal
 
-    calculated_temperatures = np.zeros((transmission.shape[1]))
+    # Initialize calculation arrays
+    Tcalc = N.zeros((absorption.shape[1]))
+    Terror = N.zeros(absorption.shape[1])
+    
     # iterate through each time point
-    for i in range(transmission.shape[1]):
-        peak = np.max(transmission[:,i])
-        errors = np.abs(peak * synthetic_profiles - transmission[:, i])
-        errors_collapsed = np.sum(errors, axis=1)
-        minimum = np.min(errors_collapsed)
-        index = np.where(errors_collapsed == minimum)
-        calculated_temperatures[i] = temperatures[index[0][0]]
-        if i == 365:
-            print "Minimum Error =", minimum
-            print "Temperature = ", temperatures[index[0][0]]
-            plt.hold(True)
-            plt.plot(wavelengths, peak * synthetic_profiles[index[0][0], :])
-            plt.plot(wavelengths+adjust, transmission[:, i])
-            plt.legend(['Synthetic'])
-            plt.show()
+    for i in range(settings['points']):
+        # TODO: Associated with normalization, should not be necessary once
+        # real metastable absorption is included
+        peak = N.max(absorption[:,i])
+        errors = N.abs(peak * absorption_synthetic - absorption[:, i])
         
-    plt.plot(calculated_temperatures)
-    plt.ylabel('Temperature (K)')
+        errors_collapsed = N.sum(errors, axis=1)
+        minimum = N.min(errors_collapsed)
+        Terror[i] = minimum/N.sum(absorption[:,i])
+        index = N.where(errors_collapsed == minimum)
+        Tcalc[i] = temperatures[index[0][0]]
+        
+        if debug:
+            if i == 0:
+                position = 230
+            if i in check:
+                position = position + 1
+                plt.subplot(position)
+                print 'Temperature (step %g):' % i, Tcalc[i]
+                print 'Error (step %g):' % i, Terror[i]
+                plt.plot(frequencies, peak * absorption_synthetic[index[0][0], :], '-k')
+                plt.plot(frequencies, absorption[:, i], '.r')
+                mtext.Text(3e9, 0.9, 'T$_g$ = %g' % Tcalc[i])
+                plt.axis([-4e9, 4e9, 0, 1])
+                t = i*settings['dt']*1e6
+                plt.title('Time = %g $\mu$s' % t)
     plt.show()
-    return calculated_temperatures
+    return Tcalc

lineshapes.py
@@ -13,7 +13,7 @@ def gaussian(fwhm):
         return 1/(sigma*sqrt(2*np.pi)) * np.exp(-0.5 * np.power(x/sigma, 2))
     return func
 
-def voigt(fwhm_d, fwhm_p):
+def voigt(fwhm_d, fwhm_a):
     """ Returns a Voigt function using wofz.
     
     This function generates a Voigt function and returns it to the user.
@@ -21,11 +21,12 @@ def voigt(fwhm_d, fwhm_p):
     more expensive than the pseudo-Voigt.
     
     Keyword arguments:
-    sigma -- Gaussian linewidth
-    gamma -- Lorentzian linewidth
+    fwhm_d -- FWHM of the Gaussian portion
+    fwhm_a -- FWHM of the Lorentzian portion
     """
     sigma = fwhm_d/(2*sqrt(2*log(2)))
-    gamma = fwhm_p/2
+    # TODO: Verify the factor of 0.5 for gamma!
+    gamma = fwhm_a/2
     def func(x):
         z = (x + 1j*gamma)/(sigma*sqrt(2))
         return wofz(z).real/(sigma*sqrt(2*np.pi))

parse.py
@@ -15,7 +15,7 @@ import numpy as np
 # Part of package
 import preprocess
     
-def config(top_dir='.'):
+def config(top_dir='.', debug=False):
     # Parse the settings file, check for proper formatting
     config = ConfigParser.RawConfigParser()
     try:
@@ -34,13 +34,19 @@ def config(top_dir='.'):
         averages = config.getint('Settings', 'Averages')
         time_domain = config.getfloat('Settings', 'Time Domain')
         pressure = config.getfloat('Settings', 'Pressure')
+        offset = config.getfloat('Settings', 'Offset')
     except ConfigParser.NoOptionError:
         print "Settings file is not properly formatted, quitting."
         sys.exit(1)
     return {'samples':samples, 'points':points, 'mod_initial':mod_initial,
-            'mod_final':mod_final, 'averages':averages, 'dt':time_domain/points}
+            'mod_final':mod_final, 'averages':averages, 'dt':time_domain/points, 'pressure':pressure, 'offset':offset}
     
-def load(dir, samples, points):
+def _load(dir, samples, points, debug=False):
+    """Generate data arrays and load data from file.
+    
+    Private method for reading in data in the format determined by my exper-
+    iment. Modify as necessary.
+    """
     # Initialize storage arrays
     pd_b = np.zeros((samples, points))
     ref_b = np.zeros((samples, points))
@@ -70,8 +76,7 @@ def load(dir, samples, points):
     
     return (pd_s, pd_b), (ref_s, ref_b)#, (v_s, v_b), (i_s, i_b)
     
-def data(dir='.', samples=1, points=1, averages=1, mod_initial=0,
-         mod_final=0, dt=1e-6, pressure=1.0):
+def data(dir='.', debug=False, **settings):
     """Custom parser for measurement files.
     
     Reads in the data related to a particular set of measurements. Assumes
@@ -87,11 +92,6 @@ def data(dir='.', samples=1, points=1, averages=1, mod_initial=0,
     """
    
     trace_dir = path.join(dir, 'Scopes')
-    
-    pd, ref = load(trace_dir, samples, points)
-                                        
-    ma2ghz = 0.8
-    wavelength_range = np.linspace(mod_initial*ma2ghz, mod_final*ma2ghz,
-                                   samples)
-    t = dt * np.arange(points)
-    return pd, wavelength_range, t
+    pd, ref = _load(trace_dir, settings['samples'], settings['points'])
+    t = settings['dt'] * np.arange(settings['points'])
+    return pd, t

preprocess.py
@@ -10,13 +10,22 @@ import gui
 # Third party
 import numpy as np
 
-def transmission(signal, reference): 
+def transmission(signal, reference, debug=False):
+    """Generates a proper transmission curve from input data.
+    
+    Method subtracts out the plasma-induced emissions (unrelated to the
+    laser) and corrects for the baseline shift resulting from increased
+    diode power.
+    """
+    # Determine 100% transmission signal and average over acquisition time
     unabsorbed = np.mean(reference[0] - reference[1], axis=1)
     
+    # Correct transmission signal for normal plasma emissions
     signal_total = signal[0]
     signal_plasma = signal[1]
-    signal_corrected = signal_total - signal_plasma
+    transmitted = signal_total - signal_plasma
     
-    baseline_adjusted = signal_corrected/unabsorbed[:, np.newaxis]
+    # Normalize transmission signal to full transmission value
+    baseline_adjusted = transmitted/unabsorbed[:, np.newaxis]
     
     return baseline_adjusted